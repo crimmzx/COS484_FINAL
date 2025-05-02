800f7f1bebda23197ead2872e3830b5539a26c4f
William Zhang <william.zhang@genesys.com>
2025-05-01 21:16:30 -0400

    RESULTS

---
+++ tracking_objecst_eval.py
@@ -0,0 +1,103 @@
+# Copyright 2022 PAL Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# need to edit this for web of lies
+
+import copy
+import json
+import argparse
+import tqdm
+import re
+import openai
+import os
+from dotenv import load_dotenv
+from pal import interface
+from pal.prompt import tracking_objects_prompt
+
+load_dotenv()
+# @param {type:"string"}
+openai.api_key = os.getenv("OPENAI_API_KEY")
+print(openai.api_key)
+MODEL = "gpt-4"  # @param {type:"string"}
+
+
+DATA_PATH = "datasets/tracking_shuffled_objects_three_objects.json"
+OUTPUT_PATH = "eval_results/tracking_objects.jsonl"
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--append", action="store_true")
+parser.add_argument("--verbose", action="store_true")
+args = parser.parse_args()
+
+examples = json.load(open(DATA_PATH))["examples"]
+task_prefix = json.load(open(DATA_PATH))["task_prefix"]
+
+itf = interface.ProgramInterface(
+    stop="\n\n\n", get_answer_symbol="answer", verbose=args.verbose
+)
+
+if args.append:
+    lines = open(OUTPUT_PATH).readlines()
+    num_skip_exps = len(lines)
+    scores = [x["score"] for x in map(json.loads, lines)]
+else:
+    num_skip_exps = 0
+    scores = []
+
+with open(OUTPUT_PATH, "a" if args.append else "w") as f:
+    pbar = tqdm.tqdm(
+        examples[num_skip_exps:], initial=num_skip_exps, total=len(examples)
+    )
+    for x in pbar:
+        question = task_prefix + x["input"]
+        result = copy.copy(x)
+
+        try:
+            print(
+                tracking_objects_prompt.TRACKING_OBJECTS_PROMPT.format(
+                    question=question
+                )
+            )
+            # ans = itf.run(
+            #     tracking_objects_prompt.TRACKING_OBJECTS_PROMPT.format(question=question)
+            # )
+            # ans =
+            ans_str = "(B)"
+            match = re.search(r"\(([A-C])\)", ans_str)
+            if match:
+                ans_str = f"({match.group(1)})"
+
+            # if isinstance(ans, float):
+            #     ans = int(ans)
+            # ans_str = str(ans)
+            result["answer_str"] = ans_str
+        except:
+            ans_str = ""
+
+        if ans_str.strip() == x["target"].strip():
+            score = 1
+        else:
+            score = 0
+
+        scores.append(score)
+
+        result["score"] = score
+        # result['generation'] = itf.history
+        f.write(json.dumps(result) + "\n")
+
+        itf.clear_history()
+        f.flush()
+
+print(f"Accuracy - {sum(scores) / len(scores)}")
